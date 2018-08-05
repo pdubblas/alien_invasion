2a5719a33acd0d96b13ab0fae81306cd71d1433d
pdubblas <paul@PAULMBP.local>
2018-08-05 16:09:43 -0700

    added ship class

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,14 +2,15 @@ import sys
 
 import pygame
 
+from settings import Settings
+
 def run_game():
     #Initialize game and create screen object.
     pygame.init()
-    screen = pygame.display.set_mode((1200,800))
+    ai_settings = Settings()
+    screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height, ai_settings.bg_color))
     pygame.display.set_caption("Alien Invasion")
 
-    #set the background color
-    bg_color = (230, 230, 230)
 
     #Start the main loop for the game.
     while True:
@@ -19,7 +20,7 @@ def run_game():
                 sys.exit()
 
         #Redraw screen during each pass through the loop.
-        screen.fill(bg_color)
+        screen.fill(ai_settings.bg_color)
 
         #make the most recently drawn screen visible
         pygame.display.flip()
