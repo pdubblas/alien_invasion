00cc6e9511e508bfc5704736197b0ae30e23faa1
pdubblas <paul@PAULMBP.local>
2018-08-06 19:29:23 -0700

    added speed factor

--- ship.py
+++ ship.py
@@ -2,9 +2,10 @@ import pygame
 
 class Ship():
 
-    def __init__(self, screen):
+    def __init__(self, ai_settings, screen):
         """Initialize the ship and set is starting position"""
         self.screen = screen
+        self.ai_settings = ai_settings
 
         #Load the ship image and get its rect
         self.image = pygame.image.load('images/ship.bmp')
@@ -18,12 +19,17 @@ class Ship():
         self.moving_right = False
         self.moving_left = False
 
+        #Store a decimal value for the ship's center
+        self.center = float(self.rect.centerx)
+
     def update(self):
         """Update the ship's position based on the movement flag."""
         if self.moving_right:
-            self.rect.centerx += 1
+            self.center += self.ai_settings.ship_speed_factor
         if self.moving_left:
-            self.rect.centerx -= 1
+            self.center -= self.ai_settings.ship_speed_factor
+        #Update rect object from self.center
+        self.rect.centerx = self.center
 
     def blitme(self):
         """Draw the ship at its current location"""
