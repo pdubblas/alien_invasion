5d4d59040e7146c43192c8f0402e257e9dd23b03
pdubblas <paul@PAULMBP.local>
2018-08-07 13:41:55 -0700

    broken from beer

--- game_functions.py
+++ game_functions.py
@@ -8,17 +8,21 @@ def check_events(ship):
         if event.type == pygame.QUIT:
             sys.exit()
         elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_RIGHT:
-                #move ship to the right
-                ship.moving_right = True
-            elif event.key == pygame.K_LEFT:
-                ship.moving_left = True
+            
 
         elif event.type == pygame.KEYUP:
             if event.key == pygame.K_RIGHT:
                 ship.moving_right = False
             elif event.key == pygame.K_LEFT:
                 ship.moving_left = False
+
+def check_keydown_events(event, ship):
+    if event.key == pygame.K_RIGHT:
+            #move ship to the right
+            ship.moving_right = True
+    elif event.key == pygame.K_LEFT:
+        ship.moving_left = True
+    
 
 
 def update_screen(ai_settings, screen, ship):
